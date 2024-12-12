4321c82bfd9b537bfca8646b6f83159e5438deb4
Jedidia Cruz <jed.cruz@mobilemindsinc.com>
2024-12-12 15:20:38 +0800

    clean code

--- routes/web.php
+++ routes/web.php
@@ -1,38 +1,36 @@
 <?php
 
-use App\Http\Controllers\ApplyJobController;
 use App\Http\Controllers\JobApplicationController;
 use App\Http\Controllers\JobController;
-use App\Http\Controllers\MemberController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\SearchController;
 use App\Http\Controllers\SessionController;
 use App\Http\Controllers\TagController;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 // Home
 Route::get('/', [JobController::class, 'index'])->name('home');
 
 // Employer
-Route::get('/jobs/create', [JobController::class, 'create'])->middleware('auth');
-Route::post('/jobs', [JobController::class, 'store'])->middleware('auth');
-Route::get('/jobs/view-applicants', [JobController::class, 'show'])->middleware('auth')->name('jobs.view-applicants');
-Route::get('/jobs/{job}/applicants', [JobController::class, 'showApplicants'])->name('jobs.applicants')->middleware('auth');
-Route::delete('/jobs/{job}', [JobController::class, 'destroy'])->middleware('auth');
-
+Route::middleware('auth')->group(function () {
+    Route::get('/jobs/create', [JobController::class, 'create']);
+    Route::post('/jobs', [JobController::class, 'store']);
+    Route::get('/jobs/view-applicants', [JobController::class, 'show'])->name('jobs.view-applicants');
+    Route::get('/jobs/{job}/applicants', [JobController::class, 'showApplicants'])->name('jobs.applicants');
+    Route::delete('/jobs/{job}', [JobController::class, 'destroy']);
+});
 
 // Member
-Route::get('/jobs/applied', [JobApplicationController::class, 'show'])
-    ->middleware('auth');
-Route::patch('/job-applications/{application}/status', [JobApplicationController::class, 'updateStatus'])
-    ->middleware('auth')->name('job-applications.status');
-Route::get('/apply/{job}', [JobApplicationController::class, 'create'])
-    ->middleware('auth')
-    ->can('apply', 'job')
-    ->name('apply-jobs');
-Route::post('/apply/{job}/application', [JobApplicationController::class, 'store'])->middleware('auth');
+Route::middleware('auth')->group(function () {
+    Route::get('/jobs/applied', [JobApplicationController::class, 'show']);
+    Route::patch('/job-applications/{application}/status', [JobApplicationController::class, 'updateStatus'])
+        ->name('job-applications.status');
+    Route::get('/apply/{job}', [JobApplicationController::class, 'create'])
+        ->can('apply', 'job')
+        ->name('apply-jobs');
+    Route::post('/apply/{job}/application', [JobApplicationController::class, 'store']);
+});
 
 // All
 Route::get('/search', SearchController::class);
